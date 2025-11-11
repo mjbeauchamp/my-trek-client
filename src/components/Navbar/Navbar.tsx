2bbcb70bbd680a81629e3a8058d93ee56a6f874f
mjbeauchamp <monica.j.beauchamp@gmail.com>
2025-11-10 19:45:42 -0700

    Basic navbar profile image styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,9 @@ import { useAuth0 } from "@auth0/auth0-react"
 import { Link } from "react-router";
 import styles from './Navbar.module.scss';
 import myTrekLogo from '../../assets/my-trek-logo-green.png';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
   const { loginWithRedirect, logout, user, isAuthenticated, isLoading } = useAuth0();
@@ -18,7 +21,9 @@ export default function Navbar() {
             </Link>
           </li>
           <li>
-            <Link to="/my-gear-lists">My Gear Lists</Link>
+            <Link to="/my-gear-lists">
+            {isAuthenticated ? <span>My Gear Lists</span> : <span>Create My List</span>}
+            </Link>
           </li>
           <li>
             <Link to="/gear-tips">Gear Tips</Link>
@@ -26,16 +31,36 @@ export default function Navbar() {
         </ul>
 
         {isAuthenticated ? (
-          <>
-            <span>Welcome, {user?.name ?? user?.email}</span>
-            {user?.picture ? <img src={user.picture} alt={user?.name ?? user?.email} /> : null}
-            <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-              Log out
-            </button>
-          </>
+          <div className={styles['profile-container']}>
+            <Menu>
+              <MenuButton className={styles['profile-menu-button']}>
+                { 
+                  user?.picture ? 
+                  <img className={styles['profile-image']} src={user.picture} alt={user?.name ?? user?.email} /> : 
+                  <div className={styles['profile-icon-container']}>
+                    <FontAwesomeIcon icon={faUser} color="white" size="xl" />
+                  </div>
+                }
+              </MenuButton>
+              <MenuItems className="navbar-menu-items" anchor="bottom end">
+                <MenuItem>
+                  <Link to="/my-gear-lists">My Gear Lists</Link>
+                </MenuItem>
+                <MenuItem>
+                  <button className="btn light" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                    Log Out
+                  </button>
+                </MenuItem>
+              </MenuItems>
+            </Menu>
+            
+            
+
+            
+          </div>
         ) : (
-          <button onClick={() => loginWithRedirect({authorizationParams: {redirect_uri: window.location.href}})}>
-            Log in
+          <button className="btn light" onClick={() => loginWithRedirect({authorizationParams: {redirect_uri: window.location.href}})}>
+            Log In
           </button>
         )}
       </nav>
