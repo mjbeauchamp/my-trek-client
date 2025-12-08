10f7bdf8f4b6aad8533c94ef4af7a0799ae681c0
mjbeauchamp <monica.j.beauchamp@gmail.com>
2025-12-08 15:52:41 -0600

    Basic backpacking article detail page article fetch and error handling

---
+++ src/components/ArticleSkeleton/ArticleSkeleton.tsx
@@ -0,0 +1,19 @@
+import styles from './ArticleSkeleton.module.scss';
+
+export default function ArticleSkeleton() {
+    return (
+        <article className={styles['article-skeleton']}>
+            <div className={styles['text-placeholder']}>
+                <div className={styles['title-placeholder']} />
+                <div className={styles['author-placeholder']} />
+            </div>
+            <div className={styles['image-placeholder']} />
+            <div className={styles['text-placeholder']}>
+                <div className={styles['line-placeholder']} />
+                <div className={styles['line-placeholder']} />
+                <div className={styles['line-placeholder']} />
+                <div className={styles['line-placeholder']} />
+            </div>
+        </article>
+    );
+}
--- src/pages/BackpackingArticlePage/BackpackingArticlePage.tsx
+++ src/pages/BackpackingArticlePage/BackpackingArticlePage.tsx
@@ -1,15 +1,93 @@
+import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router";
+import ArticleSkeleton from "../../components/ArticleSkeleton/ArticleSkeleton";
+import { ErrorAlertBlock } from "../../components/ErrorAlertBlock/ErrorAlertBlock";
+import styles from "./BackpackingArticlePage.module.scss"
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
 
 export default function BackpackingArticlePage() {
     const {articleId} = useParams();
-    const { state } = useLocation();
-    const { article } = state || {};
+    const location = useLocation();
+    const [article, setArticle] = useState(location.state?.article);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+    const [showSkeleton, setShowSkeleton] = useState(false);
+
+    useEffect(() => {
+        if (isLoading) {
+            const timeout = setTimeout(() => setShowSkeleton(true), 300);
+
+            return () => clearTimeout(timeout);
+        } else {
+            setShowSkeleton(false);
+        }   
+    }, [isLoading]);
+
+
+    useEffect(() => {
+        const fetchArticle = async () => {
+            setIsLoading(true);
+            setError('');
+        
+            try {
+                const res = await fetch(`${apiUrl}/backpacking-articles/${articleId}`);
+
+                const data = await res.json();
+
+                if (!res.ok) {
+                    throw new Error(data.message || "Server error");
+                }
+
+                if (data.title && data.content && data.imageUrl) {
+                    setArticle(data)
+                } else {
+                    throw new Error('Article data is an unexpected format.')
+                }
+            } catch (err) {
+                const message = err instanceof Error && err.message ? err.message : 'Server error fetching article';
+                console.error(message);
+                setError('There was an issue fetching the article.');
+            }
+            finally {
+                setIsLoading(false);
+            }
+        }
+
+        if (!article) {
+            // fallback to fetch by id if router state isn't available
+            fetchArticle();
+        }
+    }, [article, articleId]);
+
+    const renderContent = () => {
+        if (isLoading && showSkeleton) {
+            return <ArticleSkeleton />
+        }
+
+        if (error) return <ErrorAlertBlock message={error} />;
+
+        if (article && article.title && article.content && article.imageUrl) {
+            return (
+                <div aria-live="polite">
+                    <p>{article?.title}</p>
+                    <p>{article?.author}</p>
+                </div>
+            );
+        }
+
+        if (!isLoading && !error) {
+            return <p aria-live="polite">No article found.</p>;
+        }
+
+        return null;
+    }
+
     
     return (
-        <div>
-            SINGLE ARTICLE
-            <p>{article?.title}</p>
-            <p>{article?.author}</p>
+        <div className={`content-container ${styles['article-content']}`}>
+            {renderContent()}
         </div>
-    )
+    );
 }
