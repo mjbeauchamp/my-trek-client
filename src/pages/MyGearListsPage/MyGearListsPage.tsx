e88e534c1ac4d0ff104d2651aec897d60770d4c7
mjbeauchamp <monica.j.beauchamp@gmail.com>
2025-12-09 15:48:17 -0600

    Basic MyGearListsPage styling completed

--- src/pages/MyGearListsPage/MyGearListsPage.tsx
+++ src/pages/MyGearListsPage/MyGearListsPage.tsx
@@ -1,28 +1,39 @@
 import { Link } from "react-router";
 import { useAuth0 } from "@auth0/auth0-react";
 import GearLists from "../../components/GearLists/GearLists";
+import styles from "./MyGearListsPage.module.scss"
 
 export default function MyGearListsPage() {
     const { loginWithRedirect, logout, user, isAuthenticated, isLoading } = useAuth0();
 
     return (
-        <div>
-            <h1>MY GEAR LISTS</h1>
-            <p>Create multiple lists so that you're prepared for any adventure!</p>
-            {isAuthenticated ? <Link to="/my-gear-lists/new">Create New List</Link> : null}
+        <div className={`content-container ${styles['my-gear-lists']}`}>
+            <header>
+                <h1 className={`merriweather ${styles.header}`}>MY GEAR LISTS</h1>
+                <p className={styles.tagline}>Create multiple lists so you're prepared for any adventure!</p>
+            </header>
+
+            <hr />
+
+            {isAuthenticated ? <Link to="/my-gear-lists/new" className="btn large dark">CREATE NEW LIST</Link> : null}
 
             {
                 isAuthenticated ? 
                 <GearLists/> :
-                <div>
-                    <h2>Sign In to Start Planning</h2>
-                    <p>Log in or create your account to get started!</p>
-                    <p>Once you've signed in, you can create as many gear lists as you want</p>
+                <section className={styles['sign-in-prompt']}>
+                    <header>
+                        <h2>Sign In to Start Planning</h2>
+                    </header>
 
-                    <button className="btn light" onClick={() => loginWithRedirect({authorizationParams: {redirect_uri: window.location.href}})}>
-                        Get Started
-                    </button>
-                </div> 
+                    <section>
+                        <p>Log in or create your account to get started!</p>
+                        <p>Once you've signed in, you can create as many gear lists as you want.</p>
+
+                        <button className="btn dark large" onClick={() => loginWithRedirect({authorizationParams: {redirect_uri: window.location.href}})}>
+                            GET STARTED
+                        </button>
+                    </section>
+                </section> 
             }
             
         </div>
