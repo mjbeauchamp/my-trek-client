b614a1fe078499392f5d8e456940cf0b50618868
mjbeauchamp <monica.j.beauchamp@gmail.com>
2025-12-14 17:17:58 -0600

    Adding better error handling to fetchCommonGear function

--- src/components/GearList/CommonGearDropdown/CommonGearDropdown.tsx
+++ src/components/GearList/CommonGearDropdown/CommonGearDropdown.tsx
@@ -12,6 +12,8 @@ export default function CommonGearDropdown({ onCommonGearSelect, userGearListIte
   const [commonGear, setCommonGear] = useState<CommonGearItem[]>([]);
   const [query, setQuery] = useState('');
   const [selectedCommonGear, setSelectedCommonGear] = useState<CommonGearItem | null>(null);
+  const [hideComboBox, setHideComboBox] = useState(false);
+
   const commonGearInputRef = useRef<HTMLInputElement>(null);
 
   const categoryMetaById = Object.fromEntries(GEAR_CATEGORIES.map((cat) => [cat.id, cat]));
@@ -47,16 +49,15 @@ export default function CommonGearDropdown({ onCommonGearSelect, userGearListIte
     const fetchCommonGear = async () => {
       try {
         const res = await fetch('http://localhost:4000/api/commonGear');
-        if (!res.ok) {
-          //TODO: handle error
-        }
+        if (!res.ok) throw new Error('Network response not ok');
         const data = await res.json();
 
-        // TODO: Only set common gear if data is an array of the correct types of objects
+        if (!Array.isArray(data)) throw new Error('Improperly formatted data');
 
         setCommonGear(data);
       } catch (err) {
-        console.error('Error fetching common gear items:', err);
+        console.error('Failed to fetch common gear items:', err);
+        setHideComboBox(true);
       }
     };
     fetchCommonGear();
@@ -64,55 +65,57 @@ export default function CommonGearDropdown({ onCommonGearSelect, userGearListIte
 
   return (
     <>
-      <Combobox
-        value={selectedCommonGear}
-        onChange={(gear) => {
-          setSelectedCommonGear(gear);
-          onCommonGearSelect(gear);
-        }}
-        onClose={() => setQuery('')}
-        immediate
-      >
-        <div className="combobox-container">
-          <ComboboxInput
-            ref={commonGearInputRef}
-            placeholder="Select common items"
-            aria-label="Select Common Gear Item"
-            onChange={(e) => setQuery(e.target.value)}
-            onClick={() => {
-              commonGearInputRef.current?.blur();
-              commonGearInputRef.current?.focus();
-            }}
-            displayValue={(gear: CommonGearItem | null) => (gear ? gear.name : '')}
-            className="input-base select-base"
-          />
+      {hideComboBox ? null : (
+        <Combobox
+          value={selectedCommonGear}
+          onChange={(gear) => {
+            setSelectedCommonGear(gear);
+            onCommonGearSelect(gear);
+          }}
+          onClose={() => setQuery('')}
+          immediate
+        >
+          <div className="combobox-container">
+            <ComboboxInput
+              ref={commonGearInputRef}
+              placeholder="Select common items"
+              aria-label="Select Common Gear Item"
+              onChange={(e) => setQuery(e.target.value)}
+              onClick={() => {
+                commonGearInputRef.current?.blur();
+                commonGearInputRef.current?.focus();
+              }}
+              displayValue={(gear: CommonGearItem | null) => (gear ? gear.name : '')}
+              className="input-base select-base"
+            />
 
-          <ComboboxOptions className="combobox-options">
-            {sortedCategories.map(([category, items]) => (
-              <div key={category} className="combobox-group">
-                <h3 className="combobox-group-label">{category}</h3>
-                {items.map((gear) => {
-                  const alreadyAdded = userGearListItems?.some((item: UserGearItem) => item.name === gear.name);
-                  return (
-                    <ComboboxOption
-                      key={gear._id}
-                      value={gear}
-                      disabled={alreadyAdded}
-                      className={({ disabled }) =>
-                        `combobox-option
+            <ComboboxOptions className="combobox-options">
+              {sortedCategories.map(([category, items]) => (
+                <div key={category} className="combobox-group">
+                  <h3 className="combobox-group-label">{category}</h3>
+                  {items.map((gear) => {
+                    const alreadyAdded = userGearListItems?.some((item: UserGearItem) => item.name === gear.name);
+                    return (
+                      <ComboboxOption
+                        key={gear._id}
+                        value={gear}
+                        disabled={alreadyAdded}
+                        className={({ disabled }) =>
+                          `combobox-option
                                                     ${disabled ? 'disabled' : ''}`
-                      }
-                    >
-                      {gear.name}
-                      {alreadyAdded ? <span className="combobox-badge">Already in list</span> : null}
-                    </ComboboxOption>
-                  );
-                })}
-              </div>
-            ))}
-          </ComboboxOptions>
-        </div>
-      </Combobox>
+                        }
+                      >
+                        {gear.name}
+                        {alreadyAdded ? <span className="combobox-badge">Already in list</span> : null}
+                      </ComboboxOption>
+                    );
+                  })}
+                </div>
+              ))}
+            </ComboboxOptions>
+          </div>
+        </Combobox>
+      )}
     </>
   );
 }
